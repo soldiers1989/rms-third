0f838a6144574d829304769e83fd0257cd0f693d
xuliang <xuliang@juzifenqi.com>
2018-03-09 18:21:11 +0800

    模糊删除 redis key

--- rms-third-web/src/main/java/com/jzfq/rms/third/web/action/RedisAction.java
+++ rms-third-web/src/main/java/com/jzfq/rms/third/web/action/RedisAction.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 /**
  * @author 大连桔子分期科技有限公司
  * @date 2018/2/11 11:47.
@@ -20,21 +24,43 @@ public class RedisAction {
     @Autowired
     ICache prefixCache;
     private static String STR_REDIS_DEL_SECRET = "redisClearCount";
-    @RequestMapping(value="del.json", method= RequestMethod.GET)
+
+    @RequestMapping(value = "del.json", method = RequestMethod.GET)
     public ResponseResult deleteAll(String secret) throws BusinessException {
-        if(StringUtils.equals(STR_REDIS_DEL_SECRET,secret)){
+        if (StringUtils.equals(STR_REDIS_DEL_SECRET, secret)) {
             prefixCache.batchDel("rms_third_10");
-            return new ResponseResult("", ReturnCode.REQUEST_SUCCESS,null);
+            return new ResponseResult("", ReturnCode.REQUEST_SUCCESS, null);
         }
-        return new ResponseResult("", ReturnCode.ERROR_INVALID_ARGS,null);
+        return new ResponseResult("", ReturnCode.ERROR_INVALID_ARGS, null);
     }
 
-    @RequestMapping(value="deleteByKey.json", method= RequestMethod.GET)
+    @RequestMapping(value = "deleteByKey.json", method = RequestMethod.GET)
     public ResponseResult deleteByKey(String secret, String key) throws BusinessException {
-        if(StringUtils.equals(STR_REDIS_DEL_SECRET,secret)){
-            prefixCache.batchDel("rms_third_10"+key);
-            return new ResponseResult("", ReturnCode.REQUEST_SUCCESS,null);
+        if (StringUtils.equals(STR_REDIS_DEL_SECRET, secret)) {
+            prefixCache.batchDel("rms_third_10" + key);
+            return new ResponseResult("", ReturnCode.REQUEST_SUCCESS, null);
         }
-        return new ResponseResult("", ReturnCode.ERROR_INVALID_ARGS,null);
+        return new ResponseResult("", ReturnCode.ERROR_INVALID_ARGS, null);
     }
+
+    @RequestMapping(value = "deleteLikeKey.json", method = RequestMethod.GET)
+    public ResponseResult deleteLikeKey() throws BusinessException {
+        try {
+            File file = new File("/data/redis/redis.txt");//txt文件
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String s = null;
+            while ((s = br.readLine()) != null) {//使用readLine方法，一次读一行
+                System.out.println(s);
+                if (!"".equals(s)) {
+                    prefixCache.batchDel(s);
+                }
+            }
+            br.close();
+            ;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseResult("", ReturnCode.ACTIVE_SUCCESS, null);
+    }
+
 }

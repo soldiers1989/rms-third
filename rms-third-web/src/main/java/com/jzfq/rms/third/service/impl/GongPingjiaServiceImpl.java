8cc6a7d4239fbb7fda9b98b25866dbee74ebae33
xuliang <xuliang@juzifenqi.com>
2018-03-10 14:32:03 +0800

    获取公平价估值 如果为null  返回49999

--- rms-third-web/src/main/java/com/jzfq/rms/third/service/impl/GongPingjiaServiceImpl.java
+++ rms-third-web/src/main/java/com/jzfq/rms/third/service/impl/GongPingjiaServiceImpl.java
@@ -606,17 +606,13 @@ public class GongPingjiaServiceImpl implements IGongPingjiaService{
      */
     private ResponseResult getGongpingjiaData( Map<String, Object> params,Map<String, Object> bizParams) throws BusinessException{
         ResponseResult response = sendMessegeService.sendByThreeChance(SendMethodEnum.GPJ01.getCode(),params,bizParams);
+        response = null;
         if(response==null){
             log.info("公平价估价接口调用[车架号={} 车牌头两位={}] 响应信息为空" ,
                     bizParams.get("vin"),bizParams.get("licensePlatHeader"));
 //            throw new BusinessException(ReturnCode.ERROR_RESPONSE_NULL.code(),"公平价估价接口失败 返回为空",true);
             response = new ResponseResult();
-            JSONObject json = new JSONObject();
-            json.put("code", "200");
-            json.put("data", "49999");
-            json.put("msg", "Evaluation finished,thanks.");
-            json.put("traceID", TraceIDThreadLocal.getTraceID());
-            response.setData(json);
+
         }
         return response;
     }

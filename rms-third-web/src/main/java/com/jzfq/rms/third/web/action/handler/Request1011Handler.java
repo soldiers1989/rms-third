768eb998be1a86ecd6937bcab40e31bc8723f337
xuliang <xuliang@juzifenqi.com>
2018-06-25 18:47:32 +0800

    三方重构

--- rms-third-web/src/main/java/com/jzfq/rms/third/web/action/handler/Request1011Handler.java
+++ rms-third-web/src/main/java/com/jzfq/rms/third/web/action/handler/Request1011Handler.java
@@ -99,14 +99,6 @@ public class Request1011Handler extends AbstractRequestHandler {
      */
     private ResponseResult handler01(AbstractRequest request) throws Exception {
         String traceId = TraceIDThreadLocal.getTraceID();
-        if (1 == 1) {
-            JSONObject resultJson = new JSONObject();
-            resultJson.put("score", "-1");
-            resultJson.put("weight", "-1");
-            return new ResponseResult(traceId, ReturnCode.REQUEST_SUCCESS, resultJson);
-        }
-
-
         String orderNo = request.getParam("orderNo").toString();
         String customerType = (String) request.getParam("customerType");
         RiskPersonalInfo info = JSONObject.parseObject(request.getParam("personInfo").toString(),
